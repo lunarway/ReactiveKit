e4d7e873e84f7c15535c178a2f6312bda46b8ed5
Mads Bøgeskov <mbo@lunar.app>
2021-10-27 15:24:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,20 @@ import PackageDescription
 let package = Package(
     name: "ReactiveKit",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_10), 
+        .iOS(.v9), 
+        .tvOS(.v9), 
+        .watchOS(.v2)
     ],
     products: [
-        .library(name: "ReactiveKit", targets: ["ReactiveKit"])
+        .library(name: "ReactiveKit",
+                 targets: ["ReactiveKit"]),
+        .library(name: "ReactiveKitStatic",
+                 type: .static,
+                 targets: ["ReactiveKit"]),
+        .library(name: "ReactiveKitDynamic",
+                 type: .dynamic,
+                 targets: ["ReactiveKit"]),
     ],
     targets: [
         .target(name: "ReactiveKit", path: "Sources"),
